63e6a7edee93c981b2a438a522237526fbc5e386
M4rcioOliveira <marciooliveiracosta@outlook.com.br>
2024-03-02 09:45:16 -0300

    first commit

---
+++ src/test/java/br/com/azship/admshipping/AdmShippingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.azship.admshipping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmShippingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
